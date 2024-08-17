7806364c5683a0852bfc44c6a9d51113732ecc5d
Daehyun Jung <daehyunj1997@gmail.com>
2024-08-17 14:27:43 -0700

    Finish Landing Page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 const Landing = () =>{
     return (
         // jumbotron is from boothstrap 4
-        <div className="jumbotron">
+        <div className="jumbotron mt-5">
             <h1>Welcome to My Todo App </h1>
             <p>Sign-In and Start Building Your Todo List</p>
+            <Link to ={'/login'} className="btn btn-primary">Login</Link>
+            <Link to ={'/register'} className="btn btn-primary ml-3">Register</Link>
+
         </div>
     )
 };
